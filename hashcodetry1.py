af8a031f0c56d4dd98f7bef16c953c5cc8f1bd34
Michael Georgiadis <mikegeor@outlook.com.gr>
2018-11-05 10:45:53 +0200

    Added DataFrames and started the process of assigning rides to vehicles

--- hashcodetry1.py
+++ hashcodetry1.py
@@ -1,4 +1,26 @@
 import numpy as np
+import pandas as pd
+
+pd.set_option('display.max_columns', None)
+
+
+def distance_from_start(dataframe):
+    dataframe['ReqSteps'] = dataframe['StartIntX'] + dataframe['StartIntY']
+    return dataframe
+
+
+def replace_earliest_start(dataframe):
+    for i in range(number_of_vehicles + 1):
+        if dataframe['EarlStart'].iloc[i] < dataframe['ReqSteps'].iloc[i]:
+            dataframe['EarlStart'].iloc[i] = dataframe['ReqSteps'].iloc[i]
+    return dataframe
+
+
+def find_steps_for_finish(dataframe):
+    dataframe['Steps'] = abs(dataframe['StartIntX'] - dataframe['EndIntX']) + \
+                         abs(dataframe['StartIntY'] - dataframe['EndIntY'])
+    return dataframe
+
 
 data = np.genfromtxt('a_example.in', delimiter=' ')
 data = data.astype(int, copy=False)
@@ -6,28 +28,24 @@ data = data.astype(int, copy=False)
 # Initializing the data needed to solve the problem
 rows = data[0][0]
 columns = data[0][1]
-vehicles = data[0][2]
+number_of_vehicles = data[0][2]
 rides = data[0][3]
 bonus = data[0][4]
-latestFinish = data[0][5]
+number_of_steps = data[0][5]
 data = np.delete(data, 0, 0)
 
+dataf = pd.DataFrame(data)
 
-# Start Intersection, End Intersection, Earliest Start, Latest Finish of the array
-startInt = np.asarray([i[0:2] for i in data])
-endInt = np.asarray([i[2:4] for i in data])
-earliestStart = np.asarray(([i[4] for i in data]))
-latestFinish = np.asarray([i[5] for i in data])
+dataf.columns = ['StartIntX', 'StartIntY', 'EndIntX', 'EndIntY', 'EarlStart', 'LatFinish']
+dataf = distance_from_start(dataf)
+dataf = replace_earliest_start(dataf)
+dataf = find_steps_for_finish(dataf)
+dataf.sort_values(['EarlStart', 'ReqSteps', 'Steps'], ascending=[True, True, True], inplace=True)
+ride_list = dataf.index.tolist()
+print(ride_list)
+vehicle_list = np.zeros(number_of_vehicles, dtype=int)
 
+for i in range(number_of_vehicles):
+    vehicle_list[i] = i+1
 
-score = earliestStart+latestFinish/(abs(startInt[:, 0] - endInt[:, 0]) + abs(startInt[:, 1] - endInt[:, 1]))
-data = np.column_stack(([data, score]))
-print(data)
-
-"""
-for i in range(rides):
-    print("Start Intersection for vehicle", str(i+1), ":\n", startInt[i])
-    print("End Intersection for vehicle", str(i+1), ":\n", endInt[i])
-    print("Earliest Start for vehicle", str(i+1), ":\n", earliestStart[i])
-    print("Latest Finish for vehicle", str(i+1), ":\n", latestFinish[i])
-"""
+result_data = pd.DataFrame({'Vehicles': vehicle_list})
